c3d75bf3c2ce57ac0626b59069218b1d03f2ecaf
Geoge Sladkovsky <sladkovsky.george@gmail.com>
2024-06-01 21:28:18 +0200

    Add sign up integration tests to Identity

---
+++ Identity.IntegrationTests/ControllerTests/AccountControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using Identity.Domain.DTOs.AccountDTOs.Requests;
+using Identity.Domain.DTOs.AccountDTOs.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.IntegrationTests.ControllerTests
+{
+    public class AccountControllerTests
+    {
+        [Fact]
+        public async Task Sign_Up_Success()
+        {
+            // Arrange
+            var application = new CustomWebApplicationFactory();
+            SignUpRequest request = new SignUpRequest()
+            {
+                Email = "sladkovsky.george@test.com",
+                Password = "12345678",
+                UserName = "George"
+            };
+
+            var client = application.CreateClient();
+
+            // Act
+            var response = await client.PostAsJsonAsync("/api/v1/account/sign-up", request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var token = await response.Content.ReadFromJsonAsync<TokenDTO>();
+            token.Should().NotBeNull();
+            token.Token.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task Sign_Up_Failure()
+        {
+            // Arrange
+            var application = new CustomWebApplicationFactory();
+            SignUpRequest request = new SignUpRequest()
+            {
+                Email = "sladkovsky.george",
+                Password = "12345678",
+                UserName = "George"
+            };
+
+            var client = application.CreateClient();
+
+            // Act
+            var response = await client.PostAsJsonAsync("/api/v1/account/sign-up", request);
+
+            // Assert
+            response.IsSuccessStatusCode.Should().BeFalse();
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+    }
+}
---
+++ Identity.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,50 @@
+﻿using Identity.Infrastructure;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.IntegrationTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        override protected void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                // Remove the existing DbContextOptions
+                services.RemoveAll(typeof(DbContextOptions<IdentityServiceDbContext>));
+
+                // Register a new DBContext that will use our test connection string
+                services.AddDbContext<IdentityServiceDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                });
+
+                // Add the authentication handler
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+
+                // Delete the database (if exists) to ensure we start clean
+                IdentityServiceDbContext dbContext = CreateDbContext(services);
+                dbContext.Database.EnsureDeleted();
+            });
+        }
+
+        private static IdentityServiceDbContext CreateDbContext(IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            var scope = serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<IdentityServiceDbContext>();
+
+            return dbContext;
+        }
+    }
+}
